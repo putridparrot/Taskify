0bfc6e7145e33ba81f7ae817dc6a44ec69ff6aea
Avneesh Verma <avneesh_verma@yahoo.com>
2020-04-11 19:36:29 +0100

    [AV_F_AddWPF_1] Setting up prism 7 wpf project

---
+++ src/WpfPrismApplication/WpfTaskify/WpfTaskify/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using Unity;
+using Unity.Extension;
+using Unity.Lifetime;
+
+namespace WpfTaskify
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App: PrismApplication
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.Register<IShell, Shell>();
+            containerRegistry.Register<IShellViewModel, ShellViewModel>();
+        }
+
+        protected override Window CreateShell()
+        {
+            var shell = this.Container.Resolve<IShell>();
+            var shellViewModel = this.Container.Resolve<IShellViewModel>();
+            shell.DataContext = shellViewModel;
+            return (Window) shell;
+        }
+    }
+}
