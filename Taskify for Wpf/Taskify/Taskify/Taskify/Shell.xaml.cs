5b0d802916e3d391a7b896f61a9c0b64c86fcc95
Avneesh Verma <avneesh_verma@yahoo.com>
2020-04-12 22:10:25 +0100

    Added Code for Taskify Wpf App

---
+++ Taskify for Wpf/Taskify/Taskify/Taskify/Shell.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace WpfTaskify
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Shell : IShell
+    {
+        public Shell()
+        {
+            InitializeComponent();
+        }
+    }
+}
