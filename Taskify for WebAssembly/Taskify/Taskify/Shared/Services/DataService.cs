ad4a11882cf7fd63a87f46653a3aa3afbb584ce5
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2020-07-12 14:40:55 +0100

    Refactored project as old one wasn't deploying properly

---
+++ Taskify for WebAssembly/Taskify/Taskify/Shared/Services/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+using Taskify.Shared.Dto;
+
+namespace Taskify.Shared.Services
+{
+    public class DataService : IDataService
+    {
+        private readonly IDataServiceApi _dataServiceApi;
+
+        public DataService()
+        {
+            // TODO move URL into configuration
+            _dataServiceApi = RestService.For<IDataServiceApi>("http://localhost:52606/api/");
+        }
+
+        public Task<List<TaskList>> GetTaskLists()
+        {
+            // TODO: handle caching etc.
+            return _dataServiceApi.GetTaskLists();
+        }
+    }
+}
---
+++ Taskify for WebAssembly/Taskify/Taskify/Shared/Services/IDataService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Taskify.Shared.Dto;
+
+namespace Taskify.Shared.Services
+{
+    public interface IDataService
+    {
+        Task<List<TaskList>> GetTaskLists();
+    }
+}
