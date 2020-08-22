7261fb287c24e8aa7e28560590e24348dcb5193e
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2020-08-22 19:59:00 +0100

    Moved Taskify for Linux to Taskify for Gtk# and replace original with Avalonia UI for Linux

---
+++ Taskify for CLI/Taskify/Taskify/Arguments.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using PowerArgs;
+
+namespace Taskify
+{
+    /*
+     * As inspiration look at https://github.com/todotxt/todo.txt-cli/wiki/Usage
+     * CLI commands
+     */
+    public class Arguments
+    {
+        /// <summary>
+        /// ls (to list tasks), with arg after, use as search, i.e. list "My Task" (as well as multiple args) as &&
+        /// important - use the ls line number, i.e.important 4 to make ls item 4 important(important lines differ in colour)
+        /// </summary>
+        [ArgActionMethod, 
+         ArgDescription("Lists all tasks")]
+        public void ls()
+        {
+            Console.WriteLine("ls executed");
+        }
+
+        [ArgActionMethod, 
+         ArgDescription("Lists important tasks")]
+        public void lsi()
+        {
+            Console.WriteLine("lsi executed");
+        }
+
+        [ArgActionMethod,
+         ArgDescription("Lists assigned to you tasks")]
+        public void lsa()
+        {
+            Console.WriteLine("lsa executed");
+        }
+
+        [ArgActionMethod,
+         ArgDescription("Lists my day tasks")]
+        public void lsm()
+        {
+            Console.WriteLine("lsm executed");
+        }
+
+        [ArgActionMethod,
+         ArgDescription("Mark a task as done")]
+        public void done()
+        {
+            Console.WriteLine("done executed");
+        }
+
+        [ArgActionMethod,
+         ArgDescription("Remove a task")]
+        public void rm()
+        {
+            Console.WriteLine("rm executed");
+        }
+
+        /// <summary>
+        ///  Add "My Task" (to add a task) (should get a number assigned)
+        /// </summary>
+        [ArgActionMethod,
+         ArgDescription("Add a task")]
+        public void add()
+        {
+            Console.WriteLine("rm executed");
+        }
+
+        [HelpHook, ArgShortcut("-?"), ArgDescription("Shows help")]
+        public bool Help { get; set; }
+    }
+}
--- Taskify for CLI/Taskify/Taskify/Program.cs
+++ Taskify for CLI/Taskify/Taskify/Program.cs
@@ -1,25 +1,24 @@
 ﻿using System;
+using PowerArgs;
 
 namespace Taskify
 {
-    /*
-     * As inspiration look at https://github.com/todotxt/todo.txt-cli/wiki/Usage
-     * CLI commands
-     * add "My Task" (to add a task) (should get a number assigned)
-     * ls (to list tasks), with arg after, use as search, i.e. list "My Task" (as well as multiple args) as &&
-     * important - use the ls line number, i.e. important 4 to make ls item 4 important (important lines differ in colour)
-     * lsi - list only important tasks
-     * lsa - list assigned to you
-     * lsm - list my day
-     * done - mark task as done
-     * rm - remove task 
-     */
+
 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Args.InvokeAction<Arguments>(args);
+                //var parsed = Args.Parse<Arguments>(args);
+            }
+            catch (ArgException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ArgUsage.GenerateUsageFromTemplate<Arguments>());
+            }
         }
     }
 }
