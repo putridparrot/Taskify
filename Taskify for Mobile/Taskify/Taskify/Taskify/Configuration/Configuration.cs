e21ade7d297ef3b838818e1ee8f59bb060066ed3
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2021-11-06 10:30:45 +0000

    Updates to the mobile application, incl. item details page and config

---
+++ Taskify for Mobile/Taskify/Taskify/Taskify/Configuration/Configuration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Plugin.Settings;
+using Plugin.Settings.Abstractions;
+
+namespace Taskify.Configuration
+{
+    public static class Configuration
+    {
+        private static ISettings AppSettings => CrossSettings.Current;
+
+        public static string Url
+        {
+            get => AppSettings.GetValueOrDefault(nameof(Url), String.Empty);
+            set => AppSettings.AddOrUpdateValue(nameof(Url), value);
+        }
+
+    }
+}
---
+++ Taskify for Mobile/Taskify/Taskify/Taskify/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Taskify.Data.Domain;
+
+namespace Taskify.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public TaskItem Item { get; set; }
+        public ItemDetailViewModel(TaskItem item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
